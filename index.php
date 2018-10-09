8eb49f572153aa37c427c51e67f4528e212b2031
AndreBlack <matadam11@gmail.com>
2018-10-09 13:25:51 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Gambino infspec</title>
+        <link rel="stylesheet" href="/gambino/css/styles.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+        <nav>
+            <table>
+                <tr><th><a href="#" class="nav-link-a">Main</a></th></tr>
+                <tr><th><a href="#" class="nav-link-a">Categories</a></th></tr>
+            </table>
+        </nav>
+    </body>
+</html>
